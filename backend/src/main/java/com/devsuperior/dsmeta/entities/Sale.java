c5b71e5efcd25325ee58f757d094984bbfa544ef
christ962 <christian962c@yahoo.com.br>
2022-09-11 16:52:03 -0300

    API test

--- backend/src/main/java/com/devsuperior/dsmeta/entities/Sale.java
+++ backend/src/main/java/com/devsuperior/dsmeta/entities/Sale.java
@@ -9,7 +9,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 @Entity
 @Table(name = "tb_sales")
-public class sale {
+public class Sale {
    
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,7 +20,7 @@ public class sale {
 	private Double amount;
 	private LocalDate date;
 
-	public sale() {
+	public Sale() {
 		super();
 	}
 
